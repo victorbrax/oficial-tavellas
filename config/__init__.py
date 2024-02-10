246f8e458c8143ba4b13f1030d2f055afc9e5a3f
victorbrax <joaogomespcl@gmail.com>
2024-02-10 12:10:53 -0300

    correcao do path do database

--- config/__init__.py
+++ config/__init__.py
@@ -1,49 +1,33 @@
-# CODING: UTF8
-# CIC PORTAL
 from dotenv import find_dotenv, load_dotenv
-import json
 import os
 
 env = find_dotenv()
 load_dotenv(env)
 
-# PATHS
-PORTAL_PATH = os.getcwd()
-APP_PATH = os.path.join(PORTAL_PATH, "app")
+# Define o diretório base do aplicativo Flask
+base_dir = os.getcwd()
+APP_PATH = os.path.join(base_dir, "app")
 MODELS_PATH = os.path.join(APP_PATH, "models")
 VIEWS_PATH = os.path.join(APP_PATH, "views")
 CONTROLLERS_PATH = os.path.join(APP_PATH, "controllers")
 STATIC_PATH = os.path.join(APP_PATH, "public")
 
-CIC_ENV = os.getenv("CIC_SETTINGS")
-ERP_ENV = os.getenv("ERP_SETTINGS")
-VENDA_ENV = os.getenv("VENDA_SETTINGS")
-
-try:
-    DB_CIC = json.loads(CIC_ENV)
-    DB_ERP = json.loads(ERP_ENV)
-    DB_VENDA = json.loads(VENDA_ENV)
-except:
-    DB_CIC = {}
-    DB_ERP = {}
-    DB_VENDA = {}
-    print("Não foi possível autenticar-se aos bancos de dados PostgreSQL.")
-
 class Config(object):
-    SECRET_KEY = "c219d4e3-3ea8-4dbb-8641-C1C-P0RT4L-3ea8-4dbb-8641-8bbfc644aa18"
+    SECRET_KEY = "c219d4e3-3ea8-4dbb-8641-T4V32LL4S-P0RT4L-3ea8-4dbb-8641-8bbfc644aa18"
     CSRF_ENABLED = True
 
     MAIL_SERVER = "smtp.emailexchangeonline.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_DEFAULT_SENDER = "cic@covabra.com.br"
+    MAIL_DEFAULT_SENDER = "cic@covabra.com.br" # Alterar para credenciais do aplicativo.
     MAIL_USERNAME = "cic@covabra.com.br"
     MAIL_PASSWORD = "2yNDhV&P"
-
-    SQLALCHEMY_DATABASE_URI = "sqlite:///production.sqlite"
+    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'database', 'production.sqlite')
     SQLALCHEMY_BINDS = {
-        "DEV": "sqlite:///development.sqlite"
+        "DEV": 'sqlite:///' + os.path.join(base_dir, 'database', 'development.sqlite')
     }
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ADMIN_SWATCH = 'Slate' # https://bootswatch.com/3/
 
@@ -53,5 +37,4 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 class ProductionConfig(Config):
-    if DB_CIC:
-        FLASK_ENV = "production"
+    FLASK_ENV = "production"

fc7ed2736ba80e8aa21181c8b2434b60b3228f17
Víctor Gomes <joaogomespcl@gmail.com>
2024-08-23 12:01:38 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
 @echo off
-cmd /k "TITLE Windows Controller & cd /d B:\Portal\cic-portal\venv\Scripts & activate & SET FLASK_ENV=production & cd /d B:\Portal\cic-portal & python run.py"
+cd venv\Scripts
+call activate
+SET FLASK_ENV=production
+cd ..\..
+python run.py
